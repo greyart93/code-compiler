13233dd86e8c10bf45389061b981d823cf56d327
greyart93 <119904509+greyart93@users.noreply.github.com>
2024-10-08 19:25:16 +0530

    Update code-snippets.tsx

--- src/app/_editor/code-snippets.tsx
+++ src/app/_editor/code-snippets.tsx
@@ -1,10 +1,12 @@
 interface lang  {
+    text: string,
     javascript: string,
     python: string,
     c_cpp: string,
     java: string,
 }
 const codeSnippets : lang = { 
+        text: `Welcome to CodeVit!\nCodeVit is an Online Compiler build to execute code snippets with rich-code editing features\nGetting Started\nSelect a Language by clicking the above Dropdown list adjacent to Language\nStart writing your code\nPut Input in Input field (if your code require any input)\nFor multiple inputs e.g. name=input()\nage=input()\ngive input as:\n1\n2\ni.e.put each inputs on new line\nclick 'Run Code' button to run the code`,
         javascript: `console.log("ok")`,
         python: `print("python")`,
         c_cpp: `#include <iostream>\nusing namespace std;\n\nint main(){\n  cout << "C++" << endl;\n  return 0;\n}`,

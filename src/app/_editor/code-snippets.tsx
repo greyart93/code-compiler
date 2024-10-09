4a2fc6feb0b8b92d479fa910a7f000c892f2dabe
greyart93 <119904509+greyart93@users.noreply.github.com>
2024-10-09 06:41:04 +0530

    Update code-snippets.tsx

--- src/app/_editor/code-snippets.tsx
+++ src/app/_editor/code-snippets.tsx
@@ -6,7 +6,7 @@ interface lang  {
     java: string,
 }
 const codeSnippets : lang = { 
-        text: `Welcome to CodeVit!\nCodeVit is an Online Compiler build to execute code snippets with rich-code editing features\nGetting Started\nSelect a Language by clicking the above Dropdown list adjacent to Language\nStart writing your code\nPut Input in Input field (if your code require any input)\nFor multiple inputs e.g. name=input()\nage=input()\ngive input as:\n1\n2\ni.e.put each inputs on new line\nclick 'Run Code' button to run the code`,
+        text: `Welcome to CodeVit!\nCodeVit is an Online Compiler build to execute code snippets with rich-code editing features\n\nGetting Started\nSelect a Language by clicking the above Dropdown list adjacent to Language\nStart writing your code\nPut Input in Input field (if your code require any input)\nFor multiple inputs e.g.\nname=input()\nage=input()\ngive input as:\n1\n2\ni.e.put each inputs on new line\nclick 'Run Code' button to run the code`,
         javascript: `console.log("ok")`,
         python: `print("python")`,
         c_cpp: `#include <iostream>\nusing namespace std;\n\nint main(){\n  cout << "C++" << endl;\n  return 0;\n}`,
